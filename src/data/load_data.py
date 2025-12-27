79cb421b8f9b3c7bbe93db02d054e9cb257b686a
Abedulaziz01 <abduvaio@gmail.com>
2025-12-27 21:07:05 +0300

    Adding datetime features

--- src/data/load_data.py
+++ src/data/load_data.py
@@ -264,4 +264,12 @@ def _create_feature_transformer(categorical_cols, numeric_cols, ordinal_cols, da
     # Encode numeric features
     if numeric_cols:
         transformers.append(("numeric", OrdinalEncoder(handle_unknown="ignore")))
-        
+
+
+    # Encode ordinal features
+    if ordinal_cols:
+        transformers.append(("ordinal", OrdinalEncoder(handle_unknown="ignore")))
+
+    # Encode datetime features
+    if datetime_cols:
+        transformers.append(("datetime", OrdinalEncoder(handle_unknown="ignore")))
